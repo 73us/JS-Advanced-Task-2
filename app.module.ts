18bd351befeaf9fd254ebfab43163352f57e52c8
73us <roman.03.12.1998@gmail.com>
2023-07-27 17:16:48 +0300

    add missing files

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Lesson02TaskComponent } from './lesson02-task/lesson02-task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Lesson02TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
